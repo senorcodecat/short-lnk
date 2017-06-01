01c21b4f2a20ed0b641aec42bdc294bf15e6e663
Corey Decker <senorcodecat@gmail.com>
2017-06-01 10:59:53 -0400

    Completed short-link chapter

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
 
 
 export const Links = new Mongo.Collection('links');
@@ -9,3 +10,24 @@ if (Meteor.isServer) {
     return Links.find({userId: this.userId});
   });
 }
+
+Meteor.methods({
+  'links.insert'(url) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    new SimpleSchema ({
+      url: {
+        type: String,
+        label: 'Your link',
+        regEx: SimpleSchema.RegEx.Url
+      }
+    }).validate({url});
+
+    Links.insert({
+      url,
+      userId: this.userId
+    });
+  }
+});
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -26,7 +26,7 @@ export default class Link extends React.Component {
     e.preventDefault();
 
     if (url) {
-      Links.insert({ url, userId: Meteor.userId() });
+      Meteor.call('links.insert', url);
       this.refs.url.value = '';
     };
   }
